056e3b6dcc09a8894af1eb16fee820c30c5f8e13
Diego Ramallo <ramalldf@gmail.com>
2022-08-26 23:08:44 -0400

    feature_helpers.py: added dict output to mean_encode_features()

--- ds_helpers_lib/feature_helpers.py
+++ ds_helpers_lib/feature_helpers.py
@@ -16,8 +16,15 @@ def mean_encode_feature(input_df: pd.DataFrame, category: str, target: str, smoo
     val_counts = aggs['count']
     val_means = aggs['mean'] 
 
-    # Smooth mean values
-    smoothed_vals = (val_counts * val_means + smoothing_param * global_mean) / (val_counts + smoothing_param)
+    # Smooth mean values and apply to encode raw values
+    smoothed_map = (val_counts * val_means + smoothing_param * global_mean) / (val_counts + smoothing_param)
+    encoded_values = temp_df[category].map(smoothed_vals)
+
+    # Add map and encoding to dictionary
+    output = {
+              'mean_encoded_values': encoded_values,
+              'mean_encoding_map': smoothed_map
+             }  
 
     # Apply transformation and return
-    return temp_df[category].map(smoothed_vals)
+    return output

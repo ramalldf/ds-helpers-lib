edff44d4c7f17c55fde6d4e9b2640441eda3373c
Diego Ramallo <ramalldf@gmail.com>
2022-09-07 17:50:27 -0400

    feature_helpers.py: added mean-encoding class

--- ds_helpers_lib/feature_helpers.py
+++ ds_helpers_lib/feature_helpers.py
@@ -2,63 +2,75 @@ import numpy as np
 import pandas as pd
 
 
-def mean_encode_feature(input_df: pd.DataFrame, category: str, target: str, smoothing_param: int) -> pd.DataFrame:
-    '''Returns a list of mean-encoded values'''
-
-    # Init copy
-    temp_df = input_df.copy()
-
-    # Calculate global mean of target
-    global_mean = temp_df[target].mean()
-
-    # Calc. count and mean aggs 
-    aggs = temp_df.groupby(category)[target].agg(['count', 'mean'])
-    val_counts = aggs['count']
-    val_means = aggs['mean'] 
-
-    # Smooth mean values and apply to encode raw values
-    smoothed_map = (val_counts * val_means + smoothing_param * global_mean) / (val_counts + smoothing_param)
-    encoded_values = temp_df[category].map(smoothed_vals)
-
-    # Add map and encoding to dictionary
-    output = {
-              'mean_encoded_values': encoded_values,
-              'mean_encoding_map': smoothed_map
-             }  
-
-    # Apply transformation and return
-    return output
-
-# TODO: add  wrapper fxn 
-def mean_encode_cat_feats(input_df, feature_columns, target_col, smoothing_param, model_object=None):
-    '''Wrapper function to target encode a list of categorical feats'''
-
-    temp_df = input_df.copy()
-
-    # If no model given just map values
-    if model_object:
-
-        # Iterate through categorical columns and apply mapping
-        for i in feature_columns:
-            feature_encoding = model_object[i]['mean_encoding_map']
-            temp_df[i] = temp_df[i].map(feature_encoding)
-
-        # Return encoded df
-        return temp_df
+class MeanEncoder:
+    def __init__(self, smoothing_param):
+        self.smoothing_param = smoothing_param    
     
-    # Else iteratively build encoding map and append to cat feat map    
-    else:
-        cat_feat_map = {}
+    
+    def me_calculate(self, input_df, category, target, smoothing_param):
+        '''Returns a list of mean-encoded values. Smoothing param
+        defines the number of instances of a subgroup in the category
+        that must exist before that groups mean overcomes the global mean'''
 
-        for j in feature_columns:
-            me_object = mean_encode_feature(input_df, j, target_col, smoothing_param)
-            me_map = me_object['mean_encoding_map']
+        # Init copy
+        temp_df = input_df.copy()
+
+        # Calculate global mean of target
+        global_mean = temp_df[target].mean()
+
+        # Calc. count and mean aggs 
+        aggs = temp_df.groupby(category)[target].agg(['count', 'mean'])
+        val_counts = aggs['count']
+        val_means = aggs['mean'] 
+
+        # Smooth mean values and apply to encode raw values
+        smoothed_map = (val_counts * val_means + smoothing_param * global_mean) / (val_counts + smoothing_param)
+        encoded_values = temp_df[category].map(smoothed_map)
+
+        # Add map and encoding to dictionary
+        output = {
+                  'mean_encoded_values': encoded_values,
+                  'mean_encoding_map': smoothed_map
+                 }  
+
+        # Apply transformation and return
+        return output
+
+    def me_calculate_features(self, input_df, features_list, target_col):
+        '''Iterates through each feature column to build a reference dict that
+        has the mean encoding map for all features'''
+
+        # Initialize master encoding map and temporary df
+        master_encoding_map = {}
+        temp_df = input_df.copy()
+
+        # Calculate the mean encoding values for every feature, update values in columns
+        # and updaet master map with encodings
+        for j in features_list:
+            me_object = self.me_calculate(temp_df, j, target_col, self.smoothing_param)
+            temp_map = me_object['mean_encoding_map']
             temp_df[j] = me_object['mean_encoded_values']
 
             # Update feature map with mappings for this feature
-            cat_feat_map[i] = me_map
+            master_encoding_map[j] = temp_map
         
-        return temp_df, cat_feat_map
+        # Assign final map as an attribute
+        self.master_encoding_map = master_encoding_map
+
+
+    def me_encode_features(self, input_df, features_list):
+        '''Uses existing master encoding map to encode features in'''
+
+        # Initialize df
+        temp_df = input_df.copy()
+
+        # Iterate through categorical columns and apply mapping
+        for i in features_list:
+            feature_map = self.master_encoding_map[i]
+            temp_df[i] = temp_df[i].map(feature_map)
+
+        # Return encoded df
+        return temp_df
         
 
 

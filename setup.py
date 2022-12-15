17373fd828c779bc24fc30fb8f8c557e976d5128
Diego Ramallo <ramalldf@gmail.com>
2022-12-15 13:56:58 -0500

    setup.py: added setup.py base file

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py placed at root directory
+import pkg_resources
+from setuptools import setup
+
+setup(
+    name='ds_helpers_lib',
+    version='0.1.0',
+    author='',
+    description="This is a helper's library for data exploration and ML workflows",
+    url='',
+    keywords='',
+    python_requires='>=3.7, <4',
+    install_requires=["pandas==1.3.5",
+                      "pytest==7.2.0",
+                      "scikit-learn",
+                      "seaborn"]
+)

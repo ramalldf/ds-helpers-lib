98034685a1dbfa92955648521f0201f172751962
Diego Ramallo <ramalldf@gmail.com>
2022-12-15 16:26:39 -0500

    setup.py: added matplotlib to install_requires

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     url='',
     keywords='',
     python_requires='>=3.7, <4',
-    install_requires=["pandas==1.3.5",
+    install_requires=["matplotlib",
+                      "pandas==1.3.5",
                       "pytest==7.2.0",
                       "scikit-learn",
                       "seaborn"]

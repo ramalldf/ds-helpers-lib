d58e8a843e943bd09d8f0df81af0dec5b098325b
Diego Ramallo <ramalldf@gmail.com>
2023-02-15 01:32:13 -0500

    io_helpers.py: added load_mpl_style() to update fivethirtyeight style

--- ds_helpers/io_helpers.py
+++ ds_helpers/io_helpers.py
@@ -1,4 +1,7 @@
 from pkg_resources import resource_stream
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+
 import pandas as pd
 
 def load_classifier_predictions():
@@ -10,3 +13,19 @@ def load_classifier_predictions():
     temp_df = pd.read_csv(stream_object, index_col=0)
 
     return temp_df
+
+def load_mpl_style():
+    '''Updates plotting style'''
+
+    plt.style.use('fivethirtyeight')
+
+    # Define dict
+    new_dict = {'figure.facecolor': 'red'}
+
+    # Update style
+    rcParams.update(new_dict)
+    
+    # stream_object = resource_stream('ds_helpers', 'data/ds_helpers.mplstyle')
+
+    # # Read table into pandas dataframe
+    # plt.style.use(stream_object)

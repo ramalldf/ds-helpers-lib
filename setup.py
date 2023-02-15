8349631951cb0eac05f88450ec6b3216013f750d
Diego Ramallo <ramalldf@gmail.com>
2023-02-15 15:01:56 -0500

    setup.py: updated version and made mpl version explicit

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='ds_helpers',
-    version='0.1.0',
+    version='0.2.0',
     author='',
     description="This is a helper's library for data exploration and ML workflows",
     url='',
     keywords='',
     python_requires='>=3.7, <4',
-    install_requires=["matplotlib",
+    install_requires=["matplotlib==3.7.0",
                       "pandas==1.3.5",
                       "pytest==7.2.0",
                       "scikit-learn",

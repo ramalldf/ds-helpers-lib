89deb2036437513a14e8f4d350578095af5673b4
Diego Ramallo <ramalldf@gmail.com>
2023-07-10 09:13:25 -0400

    image_helpers.py: updated class name and added comments

--- ds_helpers/image_helpers.py
+++ ds_helpers/image_helpers.py
@@ -1,11 +1,11 @@
 from skimage.util import view_as_windows 
 from matplotlib import pyplot as plt
 
-class imagePatcher:
+class ImagePatcher:
   def __init__(self, input_arr, output_dim=(8,8)):
     '''Breaks image into patches and returns them in a single list'''
 
-    # Define step size as equal to the size of the rows
+    # Define attributes
     self.input_arr = input_arr 
     self.output_dim = output_dim 
     self.step_size = output_dim[0]
@@ -31,11 +31,14 @@ class imagePatcher:
   def show_image_patches(self, input_list, figsize):
     '''Views a patch images with patches from a 1D list'''
 
+    # Define number of rows and columns for visualized grid
     num_cols = self.output_dim[0]
     num_rows = self.output_dim[1]
 
+    # Init figure
     plt.figure(figsize=figsize)
 
+    # For each patch in patch list add image to grid one by one
     for x in range(self.num_patches): 
       plt.subplot(self.rows, self.rows, x+1)
       plt.imshow(input_list[x], interpolation="nearest", vmin=self.vmin, vmax=self.vmax)
